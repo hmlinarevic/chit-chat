90d15b3f411c9cb7a27cf87360e459e388679519
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2021-07-30 05:49:26 +0200

    refactor: refactor chat group access

--- src/components/Chat/ChatRoom.js
+++ src/components/Chat/ChatRoom.js
@@ -1,26 +1,26 @@
 import ChatDay from './ChatDay';
+import ChatForm from './ChatForm';
+import { getKey } from '../../helpers/tools';
 import classes from './ChatRoom.module.css';
 
-export default function ChatRoom({ loading, chat }) {
-	let chatKeys;
-
-	if (chat) {
-		chatKeys = chat.flatMap(chatOnDay => Object.keys(chatOnDay));
-	}
-
-	const loadingText = (
-		<span className={classes['chat-room__loading']}>loading chat...</span>
-	);
+export default function ChatRoom({ isLoading, chat, onAddComment }) {
+	const openChat = (chat, key) => chat[key];
 
 	return (
 		<section className={classes['chat-room']}>
-			{loading && loadingText}
-
-			{chat &&
-				chat.map((chatOnDay, i) => {
-					const chatContent = chatOnDay[chatKeys[i]];
-					return <ChatDay key={chatKeys[i]} chat={chatContent} />;
-				})}
+			<div className={classes['chat-room__content']}>
+				{isLoading && (
+					<span className={classes['chat-room__loading']}>loading chat...</span>
+				)}
+				{chat &&
+					chat.map(chatGroup => (
+						<ChatDay
+							key={getKey(chatGroup)}
+							chat={openChat(chatGroup, getKey(chatGroup))}
+						/>
+					))}
+			</div>
+			{chat && <ChatForm onAddComment={onAddComment} />}
 		</section>
 	);
 }
