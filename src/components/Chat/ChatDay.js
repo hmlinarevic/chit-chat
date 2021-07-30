373c43e85fedf84ed25f5753200492093dbc84a1
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2021-07-30 05:46:36 +0200

    refactor: refactor access  functions

--- src/components/Chat/ChatDay.js
+++ src/components/Chat/ChatDay.js
@@ -1,29 +1,32 @@
 import UserComment from '../User/UserComment';
-import { WEEK_DAYS } from '../../config/days';
-import { getDateFromTimestamp } from '../../helpers/date';
+import { isToday } from '../../helpers/date';
+import { getDateParts } from '../../helpers/date';
+import { getDayAndDateString } from '../../helpers/date';
 import { getCommentReplyCount } from '../../helpers/comment';
+import { addClassIfCommentIsReply } from '../../helpers/comment';
 import classes from './ChatDay.module.css';
 
 export default function ChatDay({ chat }) {
-	const firstChatEntry = chat[0];
-	const [chatDay, ...chatDate] = getDateFromTimestamp(firstChatEntry);
+	const firstChat = chat[0];
+	const [day, ...date] = getDateParts(firstChat);
+
+	// add class and draw reply line next to user avatar
+	addClassIfCommentIsReply(chat);
 
 	return (
 		<div className={classes['chat-day']}>
-			<span className={classes.date}>
-				{`${WEEK_DAYS[chatDay]}, ${chatDate.join('.')}`}
+			<span className={classes['chat-day__date']}>
+				{isToday(date) ? 'Today' : `${getDayAndDateString(day, date)}.`}
 			</span>
 
-			<div className={classes.grid}>
-				{chat.map(comment => {
-					return (
-						<UserComment
-							{...comment}
-							key={comment.timestamp}
-							timesReplied={getCommentReplyCount(comment, chat)}
-						/>
-					);
-				})}
+			<div className={classes['chat-day__grid']}>
+				{chat.map(comment => (
+					<UserComment
+						{...comment}
+						key={comment.timestamp}
+						replyCount={getCommentReplyCount(comment, chat)}
+					/>
+				))}
 			</div>
 		</div>
 	);
